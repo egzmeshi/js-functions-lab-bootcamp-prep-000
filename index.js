6d14948044756fdd064812978d77d12eb7a1fe73
Laiba Shah <lshah1@fordham.edu>
2020-08-18 22:31:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function happyHolidays(string) {
-  var greeting = "Happy Holidays!";
-  greeting = "Happy Holidays!";
-  return string;
+function happyHolidays() {
+  var sentence = 'Happy Holidays!';
+ 
+  console.log(sentence);
 }
